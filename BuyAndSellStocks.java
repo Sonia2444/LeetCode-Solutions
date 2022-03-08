49350b171fa23c1e087a3badcdcd9d76d5f9348e
Sonia Sarode <soniasarode19@gmail.com>
2022-03-08 22:11:00 +0530

    Add files via upload
    Soultion to question https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ BuyAndSellStocks.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+    public int maxProfit(int[] prices) {
+       
+        
+        int cp = prices[0];
+        int profit = 0;
+        
+        
+        for(int i=1;i<prices.length;i++){
+            
+            if(prices[i]<cp){
+                cp = prices[i];
+            }else{
+                profit = Math.max(prices[i]-cp,profit);
+            }
+            
+        }
+        
+        return profit;
+        
+    }
+}
